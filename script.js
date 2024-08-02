daecda0b2a96c105b35991182b6d3cf5cf83d881
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2024-08-01 19:54:21 -0500

    The calculator now calculates
    It needs some work still

--- script.js
+++ script.js
@@ -1,73 +1,117 @@
-
-let first;
-let operator;
-let number;
+let first = null;
+let operator = null;
+let second = null;
 
 let displayValue = document.getElementById("display-text");
 let initialZero = true;
-
+let isOperating = false;
+let newOp = true;
 
 let clearButton = document.getElementById("clear");
-clearButton.addEventListener("click", function(){
+clearButton.addEventListener("click", function () {
     displayValue.innerHTML = "0";
+    isOperating = false;
+    newOp = true;
+    first = null;
+    second = null;
+    operator = null;
+});
+
+let resultButton = document.getElementById("result");
+resultButton.addEventListener("click", function () {
+    let result = operate(operator, parseInt(first), parseInt(second));
+    displayValue.innerHTML = result;
+    first = displayValue.innerHTML;
+    second = null;
+    operator = null;
+    isOperating = false;
+    newOp = true;
+    console.log("clicked");
 });
 
 
 let numbers = document.getElementsByClassName("number");
-for(let button of numbers){
-    button.addEventListener("click", function(){
+for (let button of numbers) {
+    button.addEventListener("click", function () {
         populate(this);
     });
 }
 
-
-function populate(element) {
-    if (initialZero || displayValue.innerHTML.charAt(0) === '0') {
-        displayValue.innerHTML = element.innerHTML;
-        initialZero = false; 
-    } else {
-        displayValue.innerHTML += element.innerHTML;
-    }
+let operations = document.getElementsByClassName("operation");
+for (let op of operations) {
+    op.addEventListener("click", function () {
+        if (!isOperating) {
+            isOperating = true;
+        } else if (isOperating) {
+            let result = operate(operator, parseInt(first), parseInt(second));
+            displayValue.innerHTML = result;
+            first = displayValue.innerHTML;
+            newOp = true;
+        }
+        operator = this.innerHTML;
+    });
 }
 
-function add(a, b){
+function populate(element) {
+
+    let length = displayValue.innerHTML.length;
+
+    if (length !== 10 && !isOperating) {
+        if (initialZero || displayValue.innerHTML.charAt(0) === '0') {
+            displayValue.innerHTML = element.innerHTML;
+            initialZero = false;
+        } else {
+            displayValue.innerHTML += element.innerHTML;
+        }
+        first = displayValue.innerHTML;
+    }
+    else if (length !== 10 && isOperating) {
+        displayValue.innerHTML === "0";
+        if (newOp) {
+            displayValue.innerHTML = element.innerHTML;
+            newOp = false;
+        } else {
+            displayValue.innerHTML += element.innerHTML;
+        }
+        second = displayValue.innerHTML;
+    }
+    console.log(isOperating);
+    console.log(first);
+    console.log(operator);
+    console.log(second);
+}
+
+
+function add(a, b) {
     return a + b;
 }
 
-function subtract(a, b){
+function subtract(a, b) {
     return a - b;
 }
 
-function multiply(a, b){
+function multiply(a, b) {
     return a * b;
 }
 
-function divide(a, b){
+function divide(a, b) {
     return a / b;
 }
 
 
 
 
-function operate(operator, first, second){
+function operate(operator, first, second) {
     switch (operator) {
         case '+':
             return add(first, second);
         case '-':
             return subtract(first, second);
-        case '*':
+        case 'x':
             return multiply(first, second);
-        case '/':
+        case '÷':
             return divide(first, second);
         default:
             throw new Error("Invalid operator");
     }
-}
-
-try {
-    console.log(operate('+', 5, 3)); // 8
-    console.log(operate('/', 10, 2)); // 5
-    console.log(operate('/', 10, 0)); // Inf
-} catch (error) {
-    console.error(error.message);
 }
